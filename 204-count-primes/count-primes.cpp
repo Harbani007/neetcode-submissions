49935012c22dc830e66346ed1590296ec4424bf4
Harbani Kaur <harbani.k1@gmail.com>
2026-08-19 23:59:00 +0530

    Time: 0 ms (100.00%) | Memory: 27.7 MB (21.88%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,23 +1,38 @@
+static std::array<int, 5000001> LUT = []() {
+    std::array<int, 5000001> prime = {};
+    for(int k=2; k*k<5000001; k++) if(prime[k]==0) for(int i=k*2; i<5000001; i+=k) prime[i]=1;
+    for(int k=2,c=0; k<5000001; k++) prime[k] = (prime[k]==0)?c++:c;
+    return prime;
+}();
+
 class Solution {
 public:
-    int countPrimes(int n) {
-        if(n<=2){
-            return 0;
-        }
-        vector<char>isprime(n,1);
-        isprime[0]=isprime[1]=0;
-
-        int count=n-2;
-        for(int i=2;i*i<n;i++){
-            if(isprime[i]==1){
-                for(int j=i*i;j<n;j+=i){
-                    if(isprime[j]==1){
-                        isprime[j]=0;
-                        count--;
-                    }
-                }
-            }
-        }
-        return count;
+    
+    static inline int countPrimes(const int n) {
+        return LUT[n];
     }
 };
+
+// class Solution {
+// public:
+//     int countPrimes(int n) {
+//         if(n<=2){
+//             return 0;
+//         }
+//         vector<char>isprime(n,1);
+//         isprime[0]=isprime[1]=0;
+
+//         int count=n-2;
+//         for(int i=2;i*i<n;i++){
+//             if(isprime[i]==1){
+//                 for(int j=i*i;j<n;j+=i){
+//                     if(isprime[j]==1){
+//                         isprime[j]=0;
+//                         count--;
+//                     }
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
