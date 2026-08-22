f0c80649ac10b74c423c877cec6aba83cada9af6
Harbani Kaur <harbani.k1@gmail.com>
2026-08-22 15:27:23 +0530

    Time: 46 ms (5.41%) | Memory: 14.3 MB (6.82%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -5,14 +5,14 @@ public:
     }
 
     int maxVowels(string s, int k) {
-        string part=""; int cnt=0;int ans=0;
+        int cnt=0;int ans=0;  //string part="";
         for(int i=0;i<k;i++){
             if(isvowel(s[i])){ cnt++;}
-            part+=i;
+            //part+=i;
         }
         ans=cnt;
         for(int i=k;i<s.size();i++){
-            part+=s[i];
+            //part+=s[i];
             if(isvowel(s[i-k])){ cnt--;}
             if(isvowel(s[i])){ cnt++;}
             ans=max(ans,cnt);
