c25f164d3844db22c085d152fb03a7e2da42abc8
Harbani Kaur <harbani.k1@gmail.com>
2026-08-22 15:30:31 +0530

    Time: 11 ms (22.36%) | Memory: 13.1 MB (59.40%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,24 +1,55 @@
 class Solution {
 public:
-    bool isvowel(char c){
-        return string("aeiouAEIOU").find(c)!=string::npos;
+        inline bool isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
-
     int maxVowels(string s, int k) {
-        int cnt=0;int ans=0;  //string part="";
-        for(int i=0;i<k;i++){
-            if(isvowel(s[i])){ cnt++;}
-            //part+=i;
-        }
-        ans=cnt;
-        for(int i=k;i<s.size();i++){
-            //part+=s[i];
-            if(isvowel(s[i-k])){ cnt--;}
-            if(isvowel(s[i])){ cnt++;}
-            ans=max(ans,cnt);
-        }
+       
         
-        return ans;
+        int count=0;
+        int max_count=0;
+        for(int i =0;i<k;i++){
+            count +=isVowel(s[i]);
+        }
+        max_count=count;
+        if (max_count == k) return k;
+        for(int i =k;i<s.size();i++){
+            count += isVowel(s[i])-isVowel(s[i-k]);
+            if(count>max_count){
+                max_count=count;
+            }
+            
+        }
+        return max_count;
     }
 };
+
+
+
+
+
+
+// class Solution {
+// public:
+//     bool isvowel(char c){
+//         return string("aeiouAEIOU").find(c)!=string::npos;
+//     }
+
+//     int maxVowels(string s, int k) {
+//         int cnt=0;int ans=0;  //string part="";
+//         for(int i=0;i<k;i++){
+//             if(isvowel(s[i])){ cnt++;}
+//             //part+=s[i];
+//         }
+//         ans=cnt;
+//         for(int i=k;i<s.size();i++){
+//             //part+=s[i];
+//             if(isvowel(s[i-k])){ cnt--;}
+//             if(isvowel(s[i])){ cnt++;}
+//             ans=max(ans,cnt);
+//         }
+        
+//         return ans;
+//     }
+// };
 
