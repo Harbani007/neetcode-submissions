5a9e9fc626d549caf885311347f770fae6b1a748
Harbani Kaur <harbani.k1@gmail.com>
2026-08-26 23:57:23 +0530

    Time: 7 ms (35.90%) | Memory: 28.3 MB (84.23%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,19 +1,39 @@
+// class Solution {
+// public:
+//     vector<int> nextGreaterElements(vector<int>& nums) {
+//         stack<int>st;
+//         int n=nums.size();
+//         vector<int>ans(n,-1);
+//         for(int i=2*n-1;i>=0;i--){
+//             int curr=nums[i%n];
+//             while(!st.empty()&&st.top()<=curr){
+//                 st.pop();
+//             }
+//             if(i<n&&!st.empty()){
+//                 ans[i]=st.top();
+//             }
+//             st.push(curr);
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        stack<int>st;
-        int n=nums.size();
-        vector<int>ans(n,-1);
-        for(int i=2*n-1;i>=0;i--){
-            int curr=nums[i%n];
-            while(!st.empty()&&st.top()<=curr){
-                st.pop();
-            }
-            if(i<n&&!st.empty()){
-                ans[i]=st.top();
-            }
-            st.push(curr);
+   stack<int> st; int n= nums.size();
+    vector<int> ans(n);
+    
+    for( int i=2*n-1; i>=0; i--){
+        while(!st.empty() && st.top()<= nums[i%n]){
+            st.pop();
         }
-        return ans;
+       if(i<n){
+       ans[i]= st.empty()?-1:st.top();
+       }
+       st.push(nums[i%n]);
+    }
+    return ans;
     }
 };
