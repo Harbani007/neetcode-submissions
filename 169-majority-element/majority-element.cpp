d318c9fd4b811afb26bb831ce960ceb7309f52fe
Harbani Kaur <harbani.k1@gmail.com>
2026-09-06 01:15:17 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (75.67%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,12 +1,30 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n=nums.size()/2;
-        unordered_map<int,int> freq;
-        for(auto i:nums){
-            if(freq[i]>= n) return i;
-            freq[i]++;
+        int n=nums.size();
+        int cnt=0;
+        int candidate;
+        for(int i=0;i<n;++i){
+            if(cnt==0) candidate=nums[i];
+
+            if(candidate==nums[i]) cnt++;
+
+            else cnt--;
         }
-        return -1;
+        return candidate;
+        
     }
 };
+
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n=nums.size()/2;
+//         unordered_map<int,int> freq;
+//         for(auto i:nums){
+//             if(freq[i]>= n) return i;
+//             freq[i]++;
+//         }
+//         return -1;
+//     }
+// };
